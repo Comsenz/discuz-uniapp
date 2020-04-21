47b16073c1d580ea806e965cc7db27febf6def1e
VikcyYan <38848702+VikcyYan@users.noreply.github.com>
2020-04-21 17:32:49 +0800

    fix: adjust chat style

--- src/pages/message/chat.vue
+++ src/pages/message/chat.vue
@@ -35,38 +35,67 @@ export default {
   background-color: #ededed;
 }
 
-.chat-box__con {
-  display: flex;
-}
-
 .dialog-box-other {
   position: relative;
   width: 540rpx;
-  height: 80rpx;
+  max-height: 80rpx;
   padding: 20rpx;
   margin: 20rpx;
-  background: rgba(255, 255, 255, 1);
-  border: 1rpx solid rgba(229, 229, 229, 1);
+  background: #fff;
+  border: 1rpx solid #e5e5e5;
+  border-radius: 10rpx;
   opacity: 1;
 }
 
-// .dialog-box-other:before {
-//   position: absolute;
-//   top: 34rpx;
-//   left: -10rpx;
-//   border-top: 6px solid transparent;
-//   border-right: 10px solid #ccc;
-//   border-bottom: 6px solid transparent;
-//   content: '';
-// }
+.dialog-box-other:before {
+  position: absolute;
+  top: 30%;
+  left: -8rpx;
+  border-top: 12rpx solid transparent;
+  border-right: 20rpx solid #e5e5e5;
+  border-bottom: 12rpx solid transparent;
+  content: '';
+}
 
 .dialog-box-other:after {
   position: absolute;
-  top: 10px;
-  left: -8px;
-  border-top: 6px solid transparent;
-  border-right: 10px solid #fff;
-  border-bottom: 6px solid transparent;
+  top: 30%;
+  left: -8rpx;
+  border-top: 12rpx solid transparent;
+  border-right: 20rpx solid #fff;
+  border-bottom: 12rpx solid transparent;
+  content: '';
+}
+
+.dialog-box-me {
+  position: relative;
+  width: 540rpx;
+  max-height: 80rpx;
+  padding: 20rpx;
+  margin: 20rpx;
+  background: #d1e0ff;
+  border: 1rpx solid #a3caff;
+  border-radius: 10rpx;
+  opacity: 1;
+}
+
+.dialog-box-me:after {
+  position: absolute;
+  top: 30%;
+  right: -9rpx;
+  border-top: 12rpx solid transparent;
+  border-bottom: 12rpx solid transparent;
+  border-left: 20rpx solid #d1e0ff;
+  content: '';
+}
+
+.dialog-box-me:before {
+  position: absolute;
+  top: 30%;
+  right: -9rpx;
+  border-top: 12rpx solid transparent;
+  border-bottom: 12rpx solid transparent;
+  border-left: 20rpx solid #a3caff;
   content: '';
 }
 
@@ -79,6 +108,8 @@ export default {
   position: absolute;
   bottom: 0rpx;
   display: flex;
+  flex-direction: row;
+  justify-content: space-around;
   width: 100%;
   height: 80rpx;
   padding: 20rpx 20rpx 40rpx;
@@ -87,8 +118,6 @@ export default {
 }
 
 .uni-input {
-  flex-direction: row;
-  justify-content: flex-start;
   width: 65%;
   height: 80rpx;
   padding-left: 20rpx;
