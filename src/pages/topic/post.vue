ca1e27d9db4a9dc06d9dcfbc85f9ffad17d9db06
YangPengPc <1324901147@qq.com>
2020-04-22 15:16:52 +0800

    feature:add post page

---
+++ src/pages/topic/post.vue
@@ -0,0 +1,34 @@
+<template>
+  <view class="post-box">
+    <view class="post-box__header">
+      <view class="post-box__header-l">
+        <qui-icon name="icon-expression" />
+        <qui-icon name="icon-call" />
+      </view>
+      <text class="post-box__header-r">
+        还能输入450个字
+      </text>
+    </view>
+    <view>
+      <textarea></textarea>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+};
+</script>
+
+<style lang="scss" scoped>
+.post-box {
+  width: 100vw;
+  padding: 40rpx;
+  box-sizing: border-box;
+  &__header {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
