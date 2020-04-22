a85af8218e17360b5d2efee6e9959a0a95b5ff3f
VikcyYan <38848702+VikcyYan@users.noreply.github.com>
2020-04-22 17:41:33 +0800

    feat: add replied data

--- src/pages/message/replied.vue
+++ src/pages/message/replied.vue
@@ -1,5 +1,14 @@
 <template>
-  <view class="reply-me-box">回复我的</view>
+  <view class="replied-box">
+    <view>回复我的</view>
+    <view v-for="item in allRepliedNotifications" :key="item.user_id">
+      <view>用户名：{{ item.user_name }}</view>
+      <view>用户头像：{{ item.user_avatar }}</view>
+      <view>帖子内容：{{ item.post_content }}</view>
+      <view>created_at：{{ item.post_content }}</view>
+      <view>read_at：{{ item.post_content }}</view>
+    </view>
+  </view>
 </template>
 
 <script>
@@ -7,11 +16,28 @@ export default {
   data() {
     return {};
   },
+  onLoad() {
+    this.getRepliedNotifications();
+  },
+  computed: {
+    allRepliedNotifications() {
+      console.log('allRepliedNotifications', this.$store.getters['jv/get']('notification'));
+      return this.$store.getters['jv/get']('notification');
+    },
+  },
+  methods: {
+    getRepliedNotifications() {
+      const params = {
+        'filter[type]': 'replied',
+      };
+      this.$store.dispatch('jv/get', ['notification', { params }]);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.reply-me-box {
+.replied-box {
   background-color: #f9fafc;
 }
 </style>
