197b671fbd960094dfebe86626830e8f9aaedaf4
VikcyYan <38848702+VikcyYan@users.noreply.github.com>
2020-04-22 16:54:21 +0800

    feat: add system data

--- src/pages/message/system.vue
+++ src/pages/message/system.vue
@@ -1,5 +1,13 @@
 <template>
-  <view class="system-info-box">系统通知</view>
+  <view class="system-box">
+    <view>系统通知</view>
+    <view v-for="item in allSystemNotifications" :key="item.user_id">
+      <view>用户名：{{ item.user_name }}</view>
+      <view>帖子内容：{{ item.post_content }}</view>
+      <view>created_at：{{ item.post_content }}</view>
+      <view>read_at：{{ item.post_content }}</view>
+    </view>
+  </view>
 </template>
 
 <script>
@@ -7,11 +15,28 @@ export default {
   data() {
     return {};
   },
+  onLoad() {
+    this.getSystemNotifications();
+  },
+  computed: {
+    allSystemNotifications() {
+      console.log('allSystemNotifications', this.$store.getters['jv/get']('notification'));
+      return this.$store.getters['jv/get']('notification');
+    },
+  },
+  methods: {
+    getSystemNotifications() {
+      const params = {
+        'filter[type]': 'system',
+      };
+      this.$store.dispatch('jv/get', ['notification', { params }]);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.system-info-box {
+.system-box {
   background-color: #f9fafc;
 }
 </style>
