ef1b274edb5c469a1970f306f73751345e541006
Mr.Y <1324901147@qq.com>
2020-04-22 23:38:00 +0800

    update:update post page

--- src/pages/topic/post.vue
+++ src/pages/topic/post.vue
@@ -8,45 +8,127 @@
           size="40"
           color="#777"
         ></qui-icon>
-        <qui-icon class="post-box__hd-l__icon" name="icon-call" size="40" color="#777"></qui-icon>
+        <qui-icon
+          class="post-box__hd-l__icon"
+          name="icon-call"
+          size="40"
+          color="#777"
+          @click="callClick"
+        ></qui-icon>
       </view>
       <text class="post-box__hd-r">
-        还能输入450个字
+        {{ `还能输入${450 - textAreaValue.length}个字` }}
       </text>
     </view>
     <textarea
       class="post-box__con-text"
       placeholder="您想说的.."
       placeholder-class="textarea-placeholder"
+      v-model="textAreaValue"
+      auto-height
+      maxlength="450"
     ></textarea>
     <view class="post-box__ft">
       <text class="post-box__ft-tit">选择分类</text>
       <view class="post-box__ft-categories">
-        <qui-button type="primary" plain>产品经理</qui-button>
+        <qui-button
+          :type="item.status ? 'primary' : ''"
+          :plain="item.status"
+          v-for="(item, index) in btnData"
+          :key="index"
+          @click="checkClass(item, index)"
+        >
+          {{ item.description }}
+        </qui-button>
       </view>
-      <qui-button type="primary" size="large" @click="postClick">按钮</qui-button>
+      <qui-button type="primary" size="large" @click="postClick">发布</qui-button>
     </view>
   </view>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'Post',
+  data() {
+    return {
+      btnData: {
+        1: {
+          description: '默认分类',
+          _jv: {
+            id: 1,
+          },
+          status: true,
+        },
+        2: {
+          description: '默认分类2',
+          _jv: {
+            id: 2,
+          },
+          status: false,
+        },
+        5: {
+          description: '默认分类3',
+          _jv: {
+            id: 3,
+          },
+          status: false,
+        },
+        4: {
+          description: '默认分类4',
+          _jv: {
+            id: 4,
+          },
+          status: false,
+        },
+      },
+      textAreaValue: '',
+      checkClassData: {
+        1: {
+          description: '默认分类',
+          _jv: {
+            id: 1,
+          },
+          status: true,
+        },
+      },
+    };
+  },
+  computed: {
+    ...mapState({
+      getAtMemberData: state => state.atMember.atMemberData,
+    }),
+    allCategories() {
+      return this.$store.getters['jv/get']('categories');
+    },
+  },
   methods: {
+    callClick() {
+      uni.navigateTo({ url: '/components/qui-at-member-page/qui-at-member-page' });
+    },
     getCategories() {
-      console.log(3454);
       this.$store.dispatch('jv/get', ['categories', {}]).then(res => {
         console.log(res);
       });
     },
+    checkClass(e, index) {
+      if (!this.checkClassData[index]) {
+        this.checkClassData[index] = e;
+      } else {
+        delete this.checkClassData[index];
+      }
+      this.btnData[index].status = !this.btnData[index].status;
+    },
     postClick() {
-      console.log(9090);
-      // this.getCategories();
+      console.log(this.checkClassData);
     },
   },
   onLoad() {
-    console.log(123);
     this.getCategories();
+  },
+  onBackPress() {
+    console.log('页面返回无法触发事件，或使用页面通讯');
   },
 };
 </script>
@@ -84,12 +166,17 @@ export default {
   &__ft {
     &-tit {
       display: block;
-      margin: 30rpx 0 40rpx;
+      margin: 30rpx 0;
       font-size: $fg-f28;
       color: --color(--qui-FC-7D7979);
     }
     &-categories {
-      margin-bottom: 50rpx;
+      margin-bottom: 40rpx;
+      /deep/ .qui-button--button {
+        margin-top: 10rpx;
+        margin-right: 20rpx;
+        margin-bottom: 10rpx;
+      }
     }
   }
 }
