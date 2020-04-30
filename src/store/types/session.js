b9cc4d8c4dd3205b3b65925da2187a16fbf3b44c
leiyu <leiyu@comsenz-service.com>
2020-04-30 10:34:17 +0800

    feat: login auth

---
+++ src/store/types/session.js
@@ -0,0 +1,6 @@
+// 设置 user id
+export const SET_USER_ID = 'SET_USER_ID';
+// 查检 session 状态
+export const CHECK_SESSION = 'CHECK_SESSION';
+// 设置 access token
+export const SET_ACCESS_TOKEN = 'SET_ACCESS_TOKEN';
