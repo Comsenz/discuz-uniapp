e960fe1a3fe9f366ee15759f4c9d1094cce08162
VikcyYan <38848702+VikcyYan@users.noreply.github.com>
2020-05-09 14:41:22 +0800

    fix: fix manage/users page style

--- src/pages/manage/users.vue
+++ src/pages/manage/users.vue
@@ -1,71 +1,52 @@
 <template>
   <view class="manage-users-box">
-    <!-- 导航栏 -->
-    <uni-nav-bar left-icon="back" status-bar fixed @clickLeft="clickNavBarLeft">
-      <view slot="left" class="left-text">{{ title }}</view>
-    </uni-nav-bar>
     <!-- 搜索成员 -->
-    <uni-search-bar
-      placeholder="搜索成员"
-      :cancel-button="none"
-      bg-color="#f6f6f6"
-      @confirm="search"
-      style="color: #c6c6c6;"
-    ></uni-search-bar>
-    <!-- 成员信息 -->
-    <uni-list>
-      <uni-list-item
-        title="标题文字"
-        :show-extra-icon="true"
-        :extra-icon="{ color: '#4cd964', size: '70', type: 'contact' }"
-      ></uni-list-item>
-    </uni-list>
+    <view class=""></view>
+    <!-- 成员列表 -->
+    <view v-for="user in userList" :key="user.id">
+      <qui-avatar-cell
+        center
+        right-color="#aaa"
+        :mark="user.id"
+        :title="user.username"
+        :value="user.groupname"
+        :icon="
+          user.avatarUrl === '' ? 'https://discuz.chat/static/images/noavatar.gif' : user.avatarUrl
+        "
+      ></qui-avatar-cell>
+    </view>
   </view>
 </template>
 
 <script>
-import { uniNavBar, uniSearchBar, uniList, uniListItem } from '@dcloudio/uni-ui';
-// import { QuiAvatarCell } from '@/components/qui-avatar-cell/qui-avatar-cell';
-
 export default {
-  components: {
-    uniNavBar,
-    uniSearchBar,
-    uniList,
-    uniListItem,
-    // QuiAvatarCell,
-  },
+  components: {},
 
   data() {
     return {
-      title: '成员管理',
+      userList: [
+        { id: 1, username: '白展堂', groupname: '圈主', avatarUrl: '' },
+        { id: 2, username: '莫小贝', groupname: '合伙人', avatarUrl: '' },
+        { id: 3, username: '郭芙蓉', groupname: '好友', avatarUrl: '' },
+      ],
     };
   },
 
-  methods: {
-    // 回到上一个页面
-    clickNavBarLeft() {
-      uni.navigateBack({
-        delta: 1,
-      });
-    },
-  },
+  methods: {},
 };
 </script>
 
-<style lang="scss" scoped>
-@import '@/styles/base/variable/global.scss';
-@import '@/styles/base/reset.scss';
+<style lang="scss" scope>
+page {
+  background-color: #fff;
+}
+.manage-items {
+  padding-left: 40rpx;
+  background: #fff;
+  border-bottom: 2rpx solid #ededed;
 
-.manage-users-box {
-  height: 100%;
-  font-size: $fg-f28;
-  background-color: #f9fafc;
-
-  .left-text {
-    min-width: 250rpx;
-    font-weight: bold;
-    color: #343434;
+  .cell-item {
+    padding-right: 40rpx;
   }
 }
 </style>
