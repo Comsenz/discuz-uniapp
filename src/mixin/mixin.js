d2d6dc7665c967e625c08fd423f2b2efc8643378
leiyu <leiyu@comsenz-service.com>
2020-05-15 11:22:27 +0800

    fix: add mixin

--- src/mixin/mixin.js
+++ src/mixin/mixin.js
@@ -1,13 +1,22 @@
 import { status } from '@/library/jsonapi-vuex/index';
-import { THEME_DEFAULT } from '@/common/const';
+import { THEME_DEFAULT, SITE_PAY } from '@/common/const';
 
 module.exports = {
   data() {
     return {};
   },
-  onLoad() {
+  onLoad(params) {
     // getRect挂载到$u上，因为这方法需要使用in(this)，所以无法把它独立成一个单独的文件导出
     this.$u.getRect = this.$uGetRect;
+
+    const pages = getCurrentPages();
+    const currentPage = pages[pages.length-1];
+    if(!this.userIsPay() && currentPage.route !== 'pages/site/info') {
+      uni.redirectTo({
+        url: '/pages/site/info'
+      });
+    }
+
   },
   onReady() {
     const theme = this.$store.getters['theme/get']('currentTheme');
@@ -19,6 +28,15 @@ module.exports = {
   computed: {
     jvStatus() {
       return status.status;
+    },
+    forums() {
+      return this.$store.getters['jv/get']('forums/1');
+    },
+    user() {
+      return this.$store.getters['jv/get'](`users/${this.userId}`);
+    },
+    userId() {
+      return this.$store.getters['session/get']('userId');
     },
   },
   methods: {
@@ -40,6 +58,9 @@ module.exports = {
           .exec();
       });
     },
+    userIsPay() {
+      return this.paid && this.forums.set_site.site_mode === SITE_PAY;
+    }
   },
   onReachBottom() {
     uni.$emit('uOnReachBottom');
