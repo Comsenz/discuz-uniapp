36dc163726cd8690e19a7d920aaaa5599483b5de
gyn <920791506@qq.com>
2020-05-30 16:25:56 +0800

    fix: 首页分享内容地址

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -64,6 +64,7 @@ export default {
       const threadShare = this.$store.getters['jv/get'](`/threads/${this.nowThreadId}`);
       return {
         title: threadShare.type === 1 ? threadShare.title : threadShare.firstPost.summary,
+        path: `/pages/topic/index?id=${this.nowThreadId}`,
       };
     }
     return {
