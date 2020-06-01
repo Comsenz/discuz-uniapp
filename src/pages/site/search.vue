61143a9c58da69f34615edf56be9baceefa21ad3
lina <lina@coding.net>
2020-06-01 10:02:20 +0800

    feat: delete total

--- src/pages/site/search.vue
+++ src/pages/site/search.vue
@@ -47,7 +47,7 @@
       </view>
       <qui-no-data
         :tips="i18n.t('search.norelatedusersfound')"
-        v-if="userTotal === 0"
+        v-if="userList.length === 0"
       ></qui-no-data>
     </view>
     <view class="search-item search-item--themes" v-if="searchValue">
@@ -88,7 +88,7 @@
       </view>
       <qui-no-data
         :tips="i18n.t('search.norelatedthemesfound')"
-        v-if="themeTotal === 0"
+        v-if="themeList.length === 0"
       ></qui-no-data>
     </view>
   </qui-page>
@@ -103,8 +103,6 @@ export default {
       searchValue: '',
       userList: [],
       themeList: [],
-      userTotal: '',
-      themeTotal: '',
       pageNum: 1, // 当前页数
     };
   },
@@ -129,7 +127,6 @@ export default {
       status
         .run(() => this.$store.dispatch('jv/get', ['users', { params }]))
         .then(res => {
-          this.userTotal = res.length;
           this.userList = res;
         });
     },
@@ -152,7 +149,6 @@ export default {
       status
         .run(() => this.$store.dispatch('jv/get', ['threads', { params }]))
         .then(res => {
-          this.themeTotal = res.length;
           this.themeList = res;
         });
     },
