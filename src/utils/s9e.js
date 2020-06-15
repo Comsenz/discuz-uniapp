e20aae79e76d15b478c1ed7de11e3adb878912c9
leiyu <leiyu@comsenz-service.com>
2020-06-15 17:44:29 +0800

    fix: s9e

--- src/utils/s9e.js
+++ src/utils/s9e.js
@@ -3,20 +3,20 @@ import { DISCUZ_REQUEST_HOST } from '@/common/const';
 
 const tags = {
     topic: text => {
-        const regexp = /<span\s*id="(?<id>[^"]+)"\s*value="(?<value>\w+)"\s*>(?<string>[^<]+)<\/span>/igum;
+        const regexp = /<span\s*id="topic"\s*value="(?<value>\w+)"\s*>(?<string>[^<]+)<\/span>/igum;
         return text.replace(regexp, match => {
-            return match.replace(regexp, (content, id, value, text) => {
+            return match.replace(regexp, (content, value, text) => {
                 const href = `/pages/topic/content?id=${value}`;
-                return `<a href="${href}" class="content-${id}">${text}</a>`;
+                return `<a href="${href}" class="content-topic">${text}</a> `;
             });
         });
     },
     usermention: text => {
-        const regexp = /<span\s*id="(?<id>[^"]+)"\s*value="(?<value>\w+)"\s*>(?<string>[^<]+)<\/span>/igum;
+        const regexp = /<span\s*id="member"\s*value="(?<value>\w+)"\s*>(?<string>[^<]+)<\/span>/igum;
         return text.replace(regexp, match => {
-            return match.replace(regexp, (content, id, value, text) => {
+            return match.replace(regexp, (content, value, text) => {
                 const href = `/pages/profile/index?userId=${value}`;
-                return `<a href="${href}" class="content-${id}">${text}</a>`;
+                return `<a href="${href}" class="content-member">${text}</a> `;
             });
         });
     }
