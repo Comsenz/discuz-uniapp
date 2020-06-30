a74e5f326a88f5a4cd4a2dec297729f9b6504833
lina <lina@coding.net>
2020-06-30 18:48:15 +0800

    fix: fix pay

--- src/pages/site/info.vue
+++ src/pages/site/info.vue
@@ -329,6 +329,7 @@ export default {
                 return;
               }
               this.getOrderStatus(this.orderSn, browserType);
+              uni.hideLoading();
             }, 3000);
           }
         }
