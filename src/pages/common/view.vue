4ad5e778f531e588c5bc3999acbe14800c6a88c4
gudanxin1314 <13524405426@163.com>
2020-07-02 17:16:54 +0800

    fix: web-view

---
+++ src/pages/common/view.vue
@@ -0,0 +1,25 @@
+<template>
+  <view>
+    <web-view :webview-styles="webviewStyles" :src="encodeURI(viewUrl)"></web-view>
+  </view>
+</template>
+<script>
+export default {
+  props: {
+    // url
+    viewUrl: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      webviewStyles: {
+        progress: {
+          color: '#FF3333',
+        },
+      },
+    };
+  },
+};
+</script>
