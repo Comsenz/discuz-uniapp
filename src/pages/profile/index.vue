45ac2908f98a23cdf7eada16e8ea4d13442ba6e2
lina <lina@coding.net>
2020-07-08 10:08:41 +0800

    fix: fix style

--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -209,7 +209,7 @@ export default {
       this.$store.dispatch('jv/get', [`users/${userId}`, { params }]).then(res => {
         this.dialogId = res.dialog ? res.dialog._jv.id : 0;
         uni.setNavigationBarTitle({
-          title: `${res.username}的 ${this.i18n.t('profile.personalhomepage')}`,
+          title: `${res.username}的${this.i18n.t('profile.personalhomepage')}`,
         });
       });
     },
