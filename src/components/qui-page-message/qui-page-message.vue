467db0fc5b738b385aed0b8ec4e3a4ebe566a39f
gudanxin1314 <13524405426@163.com>
2020-07-09 14:26:59 +0800

    fix: 站点关闭时只有小程序内才能退出站点

--- src/components/qui-page-message/qui-page-message.vue
+++ src/components/qui-page-message/qui-page-message.vue
@@ -25,6 +25,7 @@
         {{ message.subtitle | closedError(forumError, forumError.code) }}
       </view>
       <!-- 退出小程序：https://uniapp.dcloud.io/component/navigator?id=navigator 2.1.0+ -->
+      <!--#ifdef MP-WEIXIN -->
       <navigator
         v-if="show && message.btnclickType == 'siteClose'"
         class="out page-message--exit"
@@ -32,11 +33,11 @@
         hover-class="none"
         target="miniProgram"
       >
-        <qui-button size="medium" @click="handleClick" class="out-btn">
+        <qui-button size="medium" class="out-btn">
           {{ message.btnTxt }}
         </qui-button>
       </navigator>
-
+      <!-- #endif -->
       <qui-button
         v-if="
           (show && message.btnclickType == 'toBack') || (show && message.btnclickType == 'tHome')
