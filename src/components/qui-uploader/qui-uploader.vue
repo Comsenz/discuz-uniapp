a200b484ca1e6314fd20c521ab183d5b1bf49dc6
lijunjun <li13754950174@163.com>
2020-07-10 15:29:42 +0800

    feat: BUG修改

--- src/components/qui-uploader/qui-uploader.vue
+++ src/components/qui-uploader/qui-uploader.vue
@@ -93,6 +93,7 @@ export default {
       formDataAppend: {},
       lastOrder: 0,
       numberdata: [],
+      indexs: '',
     };
   },
   watch: {
@@ -223,12 +224,15 @@ export default {
         name: _this.name,
         formData: formdataObj,
         success(res) {
+          console.log(res);
           if (res.statusCode >= 200 && res.statusCode < 300) {
-            _this.uploadBeforeList[index].uploadPercent = 100;
             setTimeout(() => {
-              _this.numberdata[index].state = _this.uploadBeforeList[index].uploadPercent;
+              _this.uploadBeforeList[_this.indexs].uploadPercent = 100;
+              _this.numberdata[_this.indexs].state = 100;
+              _this.uploadBeforeList[index].uploadPercent = 100;
+              _this.numberdata[index].state = 100;
             }, 500);
-            // console.log(JSON.parse(res.data), '~~~~~~~~~');
+            console.log(JSON.parse(res.data), '~~~~~~~~~');
             const resObj = {
               id: JSON.parse(res.data).data.id,
               type: JSON.parse(res.data).data.type,
@@ -250,19 +254,28 @@ export default {
             // _this.uploadBeforeList.splice(_this.uploadBeforeList.length - 1, 1);
             _this.uploadBeforeList.splice(index, 1);
             _this.uploadList.splice(index, 1);
+            _this.numberdata.splice(index, 1);
+            if (index > 0 && index === _this.uploadBeforeList.length) {
+              _this.indexs = index - 1;
+            } else if (index > 0 && index < _this.uploadBeforeList.length) {
+              _this.indexs = index;
+            } else if (index === 0) {
+              _this.indexs = 0;
+            }
             // 上传失败回调
             _this.$emit('uploadFail', res, _this.uploadList);
             return reject(res);
           }
           // 抛出接口信息
           _this.$emit('uploadSuccess', res, _this.uploadList);
-          _this.numberdata.splice(index, 1);
+          // _this.numberdata.splice(index, 1);
           return resolve(_this.uploadList);
         },
         fail(res) {
           _this.uploadBeforeList.splice(index, 1);
+          _this.indexs = index;
           _this.uploadList.splice(index, 1);
-          _this.numberdata.splice(index, 1);
+          // _this.numberdata.splice(index, 1);
           // 上传失败回调
           _this.$emit('uploadFail', res, _this.uploadList);
           return reject(res);
