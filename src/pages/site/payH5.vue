1d28a7a233085dce0c50c2ff7dcf8a1a2b15b2cc
lina <lina@coding.net>
2020-07-21 16:52:06 +0800

    fix: 调试

--- src/pages/site/payH5.vue
+++ src/pages/site/payH5.vue
@@ -2,14 +2,23 @@
   <qui-page :data-qui-theme="theme"></qui-page>
 </template>
 <script>
+let payPhone = null;
+
 export default {
   data() {
     return {
       userId: uni.getStorageSync('user_id') || 0,
     };
   },
-  onShow() {
+  onLoad() {
     this.refresh();
+    // 输入密码错误的情况
+    payPhone = setInterval(() => {
+      this.refresh();
+    }, 3000);
+  },
+  onUnload() {
+    clearInterval(payPhone);
   },
   methods: {
     refresh() {
