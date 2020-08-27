177bcc3fb9e5fa2a2f59d9f2ddd68c3db1469bb8
lxx0523 <2416388215@qq.com>
2020-08-27 14:07:40 +0800

    fix: 个人主页增加内容付费标识

--- src/components/qui-thread-item/qui-thread-item.vue
+++ src/components/qui-thread-item/qui-thread-item.vue
@@ -13,6 +13,7 @@
       :theme-time="thread.createdAt"
       :theme-content="thread.type == 1 ? thread.title : thread.firstPost.summary"
       :thread-type="thread.type"
+      :them-pay-btn="thread.price > 0"
       :media-url="thread.threadVideo && thread.threadVideo.media_url"
       :is-great="thread.firstPost.isLiked"
       :theme-like="thread.firstPost.likeCount"
