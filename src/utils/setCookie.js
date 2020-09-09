f5dff77e65e228b314b3549355618f40f821aa44
VikcyYan <38848702+VikcyYan@users.noreply.github.com>
2020-09-09 10:38:17 +0800

    feat: setCookie annotation

--- src/utils/setCookie.js
+++ src/utils/setCookie.js
@@ -1,8 +1,12 @@
 /**
  * 存cookie
+ * @param name 名字
+ * @param value 对应的值
+ * @param day 过期天数
  *
  * example:
- * setCookie()
+ * setCookie(name, value, day)
+ * 清除cookie：将day设置为-1
  */
 export const setCookie = (name, value, day) => {
   const date = new Date();
