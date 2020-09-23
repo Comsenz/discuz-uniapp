30865d84fdfa6471be74f8ffa6674f337a68b0d2
lina <lina@coding.net>
2020-09-23 16:45:39 +0800

    feat: member add disable

--- src/pages/manage/member.vue
+++ src/pages/manage/member.vue
@@ -42,7 +42,8 @@
               right-color="#aaa"
               :mark="item.id"
               :title="item.username"
-              :value="handleGroups(item)"
+              :label="handleGroups(item)"
+              :value="item.status == 1 ? i18n.t('manage.disable') : i18n.t('manage.normal')"
               :icon="item.avatarUrl"
               :is-real="item.isReal"
             >
@@ -72,7 +73,8 @@
               right-color="#aaa"
               :mark="item.id"
               :title="item.username"
-              :value="handleGroups(item)"
+              :label="handleGroups(item)"
+              :value="item.status == 1 ? i18n.t('manage.disable') : i18n.t('manage.normal')"
               :icon="item.avatarUrl"
               :is-real="item.isReal"
             >
@@ -112,6 +114,24 @@
               <view class="popup-wrap-con-text">{{ item.name }}</view>
               <view class="popup-wrap-con-line"></view>
             </view>
+            <view
+              @click="modifyGroupName({}, 1)"
+              v-if="forums.other && forums.other.can_edit_user_status"
+            >
+              <view class="popup-wrap-con-text">
+                {{ i18n.t('manage.disable') }}
+              </view>
+              <view class="popup-wrap-con-line"></view>
+            </view>
+            <view
+              @click="modifyGroupName({}, 2)"
+              v-if="forums.other && forums.other.can_edit_user_status"
+            >
+              <view class="popup-wrap-con-text">
+                {{ i18n.t('manage.clearDisable') }}
+              </view>
+              <view class="popup-wrap-con-line"></view>
+            </view>
           </view>
           <view class="popup-wrap-space"></view>
           <text class="popup-wrap-btn" @click="cancel">{{ i18n.t('home.cancel') }}</text>
@@ -123,8 +143,10 @@
 
 <script>
 import { debounce } from 'lodash';
+import forums from '@/mixin/forums';
 
 export default {
+  mixins: [forums],
   data() {
     return {
       searchText: '', // 输入的用户名
@@ -184,6 +206,10 @@ export default {
     },
     // 调用 获取所有用户组 接口
     getGroupList() {
+      if (this.forums.other && !this.forums.other.can_edit_user_group) {
+        this.groupList = [];
+        return;
+      }
       this.$store.dispatch('jv/get', 'groups').then(res => {
         if (res._jv) {
           delete res._jv;
@@ -236,16 +262,25 @@ export default {
       this.searchUser(this.searchText);
     },
     // 调用 批量修改用户的用户组 接口
-    modifyGroupName(item) {
+    modifyGroupName(item, status) {
       const data = [];
       if (this.checkAvatar && this.checkAvatar.length > 0) {
         for (let i = 0; i < this.checkAvatar.length; i += 1) {
-          data.push({
-            attributes: {
-              id: this.checkAvatar[i].id,
-              groupId: parseInt(item._jv.id, 10),
-            },
-          });
+          if (status) {
+            data.push({
+              attributes: {
+                id: this.checkAvatar[i].id,
+                status: status === 1 ? 1 : 0,
+              },
+            });
+          } else {
+            data.push({
+              attributes: {
+                id: this.checkAvatar[i].id,
+                groupId: parseInt(item._jv.id, 10),
+              },
+            });
+          }
         }
       }
       const params = [
