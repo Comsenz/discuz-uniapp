168be08600a67eb2ba1f071c2793e0bc247d29d3
lina <lina@coding.net>
2020-09-29 17:12:35 +0800

    feat: add tips

--- src/components/qui-thread-item/qui-thread-item.vue
+++ src/components/qui-thread-item/qui-thread-item.vue
@@ -95,11 +95,6 @@ export default {
       type: [Number, String],
       default: '0',
     },
-    // 是否可以点击到详情页和个人主页
-    canClick: {
-      type: Boolean,
-      default: true,
-    },
   },
   data() {
     return {
@@ -116,18 +111,12 @@ export default {
     // 内容部分点击跳转到详情页
     contentClick(id) {
       this.$emit('toTopic', id);
-      if (!this.canClick) {
-        return;
-      }
       uni.navigateTo({
         url: `/pages/topic/index?id=${id}&topicid=${this.conversationId}`,
       });
     },
     // 点击头像调转到个人主页
     headClick(id) {
-      if (!this.canClick) {
-        return;
-      }
       uni.navigateTo({
         url: `/pages/profile/index?userId=${id}`,
       });
