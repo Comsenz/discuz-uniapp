55c7c9fb9053ffce4911e6c2dfabcff56c486699
lina <lina@coding.net>
2020-10-10 12:11:04 +0800

    fix: 发现页，消息页和我的未登陆情况下都返回首页

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -168,11 +168,12 @@ export default {
     // #endif
     if (
       !this.$store.getters['session/get']('isLogin') &&
-      ['quinotice', 'quimy'].indexOf(this.currentTab) >= 0
+      ['quifind', 'quinotice', 'quimy'].indexOf(this.currentTab) >= 0
     ) {
-      uni.navigateTo({
-        url: 'pages/home/index',
-      });
+      this.setFooterIndex(0);
+      // uni.navigateTo({
+      //   url: 'pages/home/index',
+      // });
       return;
     }
 
