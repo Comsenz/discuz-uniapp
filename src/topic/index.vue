2973329bc57c2c0227c284e56e06b5059cc5d6e9
gyn <920791506@qq.com>
2020-10-19 15:06:11 +0800

    fix: topic

--- src/topic/index.vue
+++ src/topic/index.vue
@@ -1355,13 +1355,23 @@ export default {
                   this.payment = true;
                   this.answerPay = false;
                 } else if (
-                  this.user.id !== (data.question.be_user_id && data.user.id) &&
+                  this.user.id !== data.question.be_user_id || this.user.id !== data.user.id &&
+                  data.question.is_answer === 1
+                  // data.question.is_onlooker === true &&
+                  // this.forums.other.can_be_onlooker === true &&
+                  // data.onlookerState === false
+                ) {
+                  this.answerPay = true;
+                } else if (
+                  this.user.id !== data.question.be_user_id || this.user.id !== data.user.id &&
                   data.question.is_answer === 1 &&
                   data.question.is_onlooker === true &&
                   this.forums.other.can_be_onlooker === true &&
                   data.onlookerState === false
                 ) {
-                  this.answerPay = true;
+                  this.beAsk = false;
+                  this.payment = true;
+                  this.answerPay = false;
                 }
               } else if (data.question.price > '0.00') {
                 console.log('付费');
