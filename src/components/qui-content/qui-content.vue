1e1d52c42764843c0caa07fbb75880862b4f1ebb
gudanxin1314 <13524405426@163.com>
2020-11-23 17:25:14 +0800

    fix: 商品帖价格Bug修复

--- src/components/qui-content/qui-content.vue
+++ src/components/qui-content/qui-content.vue
@@ -164,8 +164,8 @@
               {{ postGoods.title }}
             </view>
             <view class="themeItem__content__good__ft">
-              <view class="themeItem__content__good__price" v-if="Number(postGoods.price) > 0">
-                ￥{{ postGoods.price }}元
+              <view class="themeItem__content__good__price" v-if="dataGoodInfoPrice !== ''">
+                ￥{{ dataGoodInfoPrice }}元
               </view>
             </view>
           </view>
@@ -704,6 +704,7 @@ export default {
       date: 1,
       blocKwidth: 224,
       isWeixin: false,
+      postGoodsPrice: 0, // 商品价格
     };
   },
   computed: {
@@ -724,6 +725,11 @@ export default {
     }),
   },
   created() {
+    if (this.postGoods.price.indexOf('-') !== -1 || Number(this.postGoods.price) > 0) {
+      this.dataGoodInfoPrice = this.postGoods.price;
+    } else if (Number(this.postGoods.price) <= 0) {
+      this.dataGoodInfoPrice = '';
+    }
     // #ifdef  H5
     const { isWeixin } = appCommonH.isWeixin();
     this.isWeixin = isWeixin;
