9a982701e2afa3ede491bffb8754d70d3aa6fad3
LLLLLamHo <linhao.4zhong@163com>
2020-11-24 20:58:06 +0800

    docs: 添加注释

--- src/components/qui-page-home-h5/qui-page-home-h5.vue
+++ src/components/qui-page-home-h5/qui-page-home-h5.vue
@@ -1128,6 +1128,7 @@ export default {
       // 首页主题置顶列表
       this.loadThreadsSticky();
       // 首页主题内容列表
+      // 在安卓手机的微信浏览器打开页面，使用微信登陆的时候，会出现跳转后因为首页渲染时，首页并不是展示中，导致虚拟滚动计算问题，所以需要判断是否延迟写入数据。
       this.$nextTick(() => {
         if (this.showHome && window.location.pathname === '/') {
           this.loadThreads();
