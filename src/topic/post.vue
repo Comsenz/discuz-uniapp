10c908879ef0fcfc669bfa5f3609026350e5427e
gudanxin1314 <13524405426@163.com>
2020-11-26 14:08:59 +0800

    fix: 解决内容中图片删不掉bug

--- src/topic/post.vue
+++ src/topic/post.vue
@@ -2628,7 +2628,7 @@ export default {
           }
           threads.free_words = this.word;
           posts._jv.relationships.attachments = {
-            data: this.addImg().data.concat(this.addFile().data),
+            data: this.addFile().data,
           };
           break;
         case 2:
